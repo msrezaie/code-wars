ce4e671e0d9d94872e82fd34c6ec524dbfd735b0
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-07-30 16:23:14 -0400

    5kyu challenge solution
    solved in py and js

---
+++ 5kyu/extract-the-domain-name-from-a-url.js
@@ -0,0 +1,20 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+// my solution
+function domainName(url) {
+  return url
+    .match(/(?:(?:https?:\/\/)?(?:www\.)?)?([^./]+(?:\.[^./]+)+)/)[1]
+    .split(".")[0];
+}
+
+// clever solution
+function domainName(url) {
+  url = url.replace("https://", "");
+  url = url.replace("http://", "");
+  url = url.replace("www.", "");
+  return url.split(".")[0];
+}
